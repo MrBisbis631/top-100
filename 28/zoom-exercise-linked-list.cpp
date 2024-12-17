be70e64682219ca7a2b72aa5f941688ec20c9730
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-17 20:29:33 +0200

    Refactor main function to demonstrate linked list sorting and free allocated memory

--- 28/zoom-exercise-linked-list.cpp
+++ 28/zoom-exercise-linked-list.cpp
@@ -40,24 +40,58 @@ void sort(info *&head)
     head = sorted;
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
-    info *head{nullptr};
+    info *head = new info(3);
+    head->next = new info(1);
+    head->next->next = new info(2);
+    head->next->next->next = new info(4);
 
-    int number;
-    while (std::cin >> number)
-    {
-        info *new_node = new info(number);
-        new_node->next = head;
-        head = new_node;
-    }
-
-    sort(head);
-
+    std::cout << "Original list: ";
     for (info *node = head; node != nullptr; node = node->next)
     {
         std::cout << node->_data << " ";
     }
+    std::cout << std::endl;
+
+    sort(head);
+
+    std::cout << "Sorted list: ";
+    for (info *node = head; node != nullptr; node = node->next)
+    {
+        std::cout << node->_data << " ";
+    }
+    std::cout << std::endl;
+
+    // Free allocated memory
+    while (head != nullptr)
+    {
+        info *temp = head;
+        head = head->next;
+        delete temp;
+    }
 
     return 0;
 }
+
+// int main(int argc, char const *argv[])
+// {
+//     info *head{nullptr};
+
+//     int number;
+//     while (std::cin >> number)
+//     {
+//         info *new_node = new info(number);
+//         new_node->next = head;
+//         head = new_node;
+//     }
+
+//     sort(head);
+
+//     for (info *node = head; node != nullptr; node = node->next)
+//     {
+//         std::cout << node->_data << " ";
+//     }
+
+//     return 0;
+// }
